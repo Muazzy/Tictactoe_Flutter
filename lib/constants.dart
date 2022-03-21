3cc90b63493bd604eef86e08107c76ae61b2b334
muazzam <muazzamafaque@gmail.com>
2022-03-22 01:54:33 +0500

    uploading project

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color kforegroundclr = Colors.grey.shade100;
+// Color kxclr = const Color.fromARGB(255, 219, 161, 54);
+Color kxclr = Colors.green;
+// Color koclr = const Color(0xffd81159);
+Color koclr = Colors.red;
+// Color kbgclr = const Color(0xFFFFC3D3);
+Color kbgclr = Colors.grey.shade800;
+// Color kgridclr = const Color(0xFFFFB3C6);
+Color kgridclr = Colors.grey.shade600;
+Color ktxtclr = Colors.grey.shade50;
